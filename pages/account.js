5c1282cf21daa6b38e35b28ea085de39b25c7444
Christopher Maverick <cmaverickce@gmail.com>
2017-04-24 09:13:40 -0400

    completed

--- pages/account.js
+++ pages/account.js
@@ -2,36 +2,49 @@ import React from 'react'
 import posts from '../data/posts'
 import * as _ from 'lodash'
 import Link from 'next/link'
+import AuthService from '../utils/AuthService'
 
-export default ({ url: {query: {id} } }) => {
-    const item = _.find(posts, {id: id })
 
-    return (
-        <div className='main'>
-            <Link href="/">Home</Link>
-            <div className='header'>
-                <h3> NEXTHRONE - THE REVELATION OF GAME OF THRONES' CHARACTERS </h3>
-            </div>
 
-            <div className='panel'>
-                <h1 className='heading'>
-                    Character: { item.codeName }
-                    <br />
-                    <br />
-                    Real Name: { item.realName }
-                    <br />
-                    <br />
-                    Brief Description:
-                    <br />
-                    <br />
-                    <span>{ item.story }</span>
-                </h1>
-            </div>
+export default class extends React.Component {
 
-            <div className="'singlePhoto">
-                <img src={ item.display_src } alt={item.realName} width={500} height={500} />
-            </div>
-            <style jsx>{`
+    componentDidMount() {
+        this.auth = new AuthService('c0dPqZTahGrJ4R4sbrZp4YvaY8jTsSZ5', 'maverick5.auth0.com' );
+        if(!this.auth.loggedIn()) {
+            this.props.url.replaceTo('/')
+        }
+    }
+
+    render() {
+
+        const item = _.find(posts, { id: this.props.url.query.id })
+
+        return (
+            <div className='main'>
+                <Link href="/">Home</Link>
+                <div className='header'>
+                    <h3> NEXTHRONE - THE REVELATION OF GAME OF THRONES' CHARACTERS </h3>
+                </div>
+
+                <div className='panel'>
+                    <h1 className='heading'>
+                        Character: { item.codeName }
+                        <br />
+                        <br />
+                        Real Name: { item.realName }
+                        <br />
+                        <br />
+                        Brief Description:
+                        <br />
+                        <br />
+                        <span>{ item.story }</span>
+                    </h1>
+                </div>
+
+                <div className="'singlePhoto">
+                    <img src={ item.display_src } alt={item.realName} width={500} height={500} />
+                </div>
+                <style jsx>{`
                     .main {
                         padding: 50px;
                     }
@@ -54,7 +67,9 @@ export default ({ url: {query: {id} } }) => {
                         font: 15px Monaco
                     }
                 `}</style>
-        </div>
+            </div>
 
-    )
-}
+        )
+    }
+    }
+
